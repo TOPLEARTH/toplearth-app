03c624a3f9e9848f6f058b227d2014cb594b32c4
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2024-11-13 07:05:08 +0900

    Chore: ENV Setting

--- lib/app/env/dev/dev_environment.g.dart
+++ lib/app/env/dev/dev_environment.g.dart
@@ -9,9 +9,53 @@ part of 'dev_environment.dart';
 // coverage:ignore-file
 // ignore_for_file: type=lint
 final class _DevEnvironment {
-  static const List<int> _enviedkeyAPI_SERVER_URL = <int>[];
+  static const List<int> _enviedkeyAPI_SERVER_URL = <int>[
+    3344182508,
+    622840092,
+    844710410,
+    3350741679,
+    165634816,
+    2028718038,
+    106654804,
+    3663233032,
+    508234630,
+    2291922057,
+    3376680094,
+    2009132875,
+    3223271146,
+    3355539588,
+    920674210,
+    57121707,
+    923193539,
+    2822352466,
+    2366278288,
+    2208423185,
+    3770151796,
+  ];
 
-  static const List<int> _envieddataAPI_SERVER_URL = <int>[];
+  static const List<int> _envieddataAPI_SERVER_URL = <int>[
+    3344182404,
+    622840168,
+    844710526,
+    3350741727,
+    165634874,
+    2028718073,
+    106654843,
+    3663233124,
+    508234729,
+    2291922154,
+    3376680191,
+    2009132839,
+    3223271042,
+    3355539691,
+    920674257,
+    57121759,
+    923193593,
+    2822352490,
+    2366278304,
+    2208423209,
+    3770151748,
+  ];
 
   static final String API_SERVER_URL = String.fromCharCodes(List<int>.generate(
     _envieddataAPI_SERVER_URL.length,
