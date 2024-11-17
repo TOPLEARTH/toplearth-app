409844d0778e81f26178645a5b278f355a1a8e7b
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2024-11-17 17:13:38 +0900

    Chore: Update Dependencies

--- lib/presentation/view/root/root_screen.dart
+++ lib/presentation/view/root/root_screen.dart
@@ -1,20 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:toplearth/MessagePage.dart';
 import 'package:toplearth/app/config/color_system.dart';
 import 'package:toplearth/app/config/font_system.dart';
-import 'package:toplearth/presentation/view_model/root/root_view_model.dart';
+import 'package:toplearth/local_push_notifications.dart';
 
-class RootScreen extends GetView<RootViewModel> {
+class RootScreen extends StatefulWidget {
   const RootScreen({super.key});
 
   @override
+  State<RootScreen> createState() => _RootScreenState();
+}
+
+class _RootScreenState extends State<RootScreen> {
+  @override
+  void initState() {
+    super.initState();
+    LocalPushNotifications.init();  // 푸시 알림 초기화
+    LocalPushNotifications.notificationStream.stream.listen((payload) {
+      if (payload.isNotEmpty) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MessagePage(payload: payload),
+          ),
+        );
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: ColorSystem.white,
       body: Center(
-        child: Text(
-          'Toplearth Root Screen',
-          style: FontSystem.H6,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Toplearth Root Screen',
+              style: FontSystem.H6,
+            ),
+            ElevatedButton(
+              onPressed: () {
+
+                LocalPushNotifications.showSimpleNotification(
+                  title: '투플러스',
+                  body: '10시 플로깅 대전 매칭에 성공했습니다!',
+                  payload: '일반 알림 데이터',
+                );
+              },
+              child: const Text("일반 푸시 알림"),
+            ),
+          ],
         ),
       ),
     );
