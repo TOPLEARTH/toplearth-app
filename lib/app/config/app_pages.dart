6b3e8f4f94cab8db8358dc7d4db45b25c22d08a5
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2024-11-24 01:06:53 +0900

    Feat: Add Pages, Routes, Dependency

--- lib/app/config/app_pages.dart
+++ lib/app/config/app_pages.dart
@@ -2,9 +2,12 @@ import 'package:get/get.dart';
 import 'package:toplearth/app/config/app_routes.dart';
 import 'package:toplearth/app/middleware/login_middleware.dart';
 import 'package:toplearth/app/middleware/on_boarding_middleware.dart';
+import 'package:toplearth/presentation/view/legacy/legacy_screen.dart';
 import 'package:toplearth/presentation/view/login/login_screen.dart';
 import 'package:toplearth/presentation/view/on_boarding/on_boarding_screen.dart';
 import 'package:toplearth/presentation/view/root/root_screen.dart';
+import 'package:toplearth/presentation/view_model/home/home_binding.dart';
+import 'package:toplearth/presentation/view_model/legacy/legacy_binding.dart';
 import 'package:toplearth/presentation/view_model/login/login_binding.dart';
 import 'package:toplearth/presentation/view_model/root/root_binding.dart';
 
@@ -12,22 +15,30 @@ import 'package:toplearth/presentation/view_model/root/root_binding.dart';
 abstract class AppPages {
   static List<GetPage> data = [
     GetPage(
+      name: AppRoutes.ON_BOARDING,
+      page: () => const OnBoardingScreen(),
+    ),
+    GetPage(
+      name: AppRoutes.LOGIN,
+      page: () => const LoginScreen(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
       name: AppRoutes.ROOT,
       page: () => const RootScreen(),
-      binding: RootBinding(),
+      bindings: [
+        RootBinding(),
+      ],
       middlewares: [
         OnBoardingMiddleware(),
         LoginMiddleware(),
       ],
     ),
-      GetPage(
-        name: AppRoutes.LOGIN,
-        page: () => const LoginScreen(),
-        binding: LoginBinding(),
-      ),
-      GetPage(
-        name: AppRoutes.ON_BOARDING,
-        page: () => const OnBoardingScreen(),
-      ),
+    GetPage(
+      name: AppRoutes.LEGACY,
+      page: () => const LegacyScreen(),
+      binding: LegacyBinding(),
+    )
+
   ];
 }
