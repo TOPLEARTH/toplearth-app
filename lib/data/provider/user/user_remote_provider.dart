2cc23b6f9f5bf9d2d6e288c081c6f84804b4827d
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2024-11-25 05:48:31 +0900

    Chore: Add API comment

--- lib/data/provider/user/user_remote_provider.dart
+++ lib/data/provider/user/user_remote_provider.dart
@@ -1,5 +1,7 @@
 import 'package:toplearth/core/wrapper/response_wrapper.dart';
 
 abstract class UserRemoteProvider {
+  // 사용자 정보 조회
+  // /api/v1/users/me
   Future<ResponseWrapper> getUserInformation();
 }
