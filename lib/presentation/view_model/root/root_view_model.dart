baa94ca44907981003da246d0ac7a410eff5dec8
kyujenius <hkj020607@gmail.com>
2024-11-27 01:28:57 +0900

    Refactor: root_view_model 수정

--- lib/presentation/view_model/root/root_view_model.dart
+++ lib/presentation/view_model/root/root_view_model.dart
@@ -7,14 +7,11 @@ import 'package:toplearth/app/utility/notification_util.dart';
 import 'package:toplearth/core/wrapper/state_wrapper.dart';
 import 'package:toplearth/domain/entity/global/legacy_info_state.dart';
 import 'package:toplearth/domain/entity/global/region_ranking_info_state.dart';
-import 'package:toplearth/domain/entity/global/region_ranking_state.dart';
 import 'package:toplearth/domain/entity/group/team_info_state.dart';
 import 'package:toplearth/domain/entity/plogging/plogging_info_state.dart';
-import 'package:toplearth/domain/entity/plogging/team_info_state.dart';
 import 'package:toplearth/domain/entity/quest/quest_info_state.dart';
 import 'package:toplearth/domain/entity/user/boot_strap_state.dart';
 import 'package:toplearth/domain/entity/user/user_state.dart';
-import 'package:toplearth/domain/repository/user/user_repository.dart';
 import 'package:toplearth/domain/usecase/user/read_user_state_usecase.dart';
 
 class RootViewModel extends GetxController {
@@ -51,7 +48,8 @@ class RootViewModel extends GetxController {
   TeamInfoState get teamInfoState => _teamInfoState.value;
   PloggingInfoState get ploggingInfoState => _ploggingInfoState.value;
   LegacyInfoState get legacyInfoState => _legacyInfoState.value;
-  RegionRankingInfoState get regionRankingInfoState => _regionRankingInfoState.value;
+  RegionRankingInfoState get regionRankingInfoState =>
+      _regionRankingInfoState.value;
 
   RxBool isBootstrapLoaded = false.obs;
 
@@ -63,7 +61,7 @@ class RootViewModel extends GetxController {
     _readBootStrapUseCase = Get.find<ReadBootStrapUseCase>();
 
     _userState = UserState.initial().obs;
-    _questInfoState = QuestInfoState.initial().obs;
+    _questInfoState = QuestInfoState.initial.obs;
     _teamInfoState = TeamInfoState.initial().obs;
     _ploggingInfoState = PloggingInfoState.initial().obs;
     _legacyInfoState = LegacyInfoState.initial().obs;
@@ -89,8 +87,6 @@ class RootViewModel extends GetxController {
   void onReady() async {
     _fetchBootstrapInformation();
     super.onReady();
-
-
   }
 
   void _fetchBootstrapInformation() async {
