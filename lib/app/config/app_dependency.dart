a56306397256737e6be87656e8b3bfbbfb45a877
DOHYEONG LEE <rlrlfhtm5@gmail.com>
2024-11-28 05:14:12 +0900

    Feat: Add Plogging Dependency

--- lib/app/config/app_dependency.dart
+++ lib/app/config/app_dependency.dart
@@ -3,13 +3,17 @@ import 'package:toplearth/data/provider/auth/auth_remote_provider.dart';
 import 'package:toplearth/data/provider/auth/auth_remote_provider_impl.dart';
 import 'package:toplearth/data/provider/group/group_remote_provider.dart';
 import 'package:toplearth/data/provider/group/group_remote_provider_impl.dart';
+import 'package:toplearth/data/provider/plogging/plogging_remote_provider.dart';
+import 'package:toplearth/data/provider/plogging/plogging_remote_provider_impl.dart';
 import 'package:toplearth/data/provider/user/user_remote_provider.dart';
 import 'package:toplearth/data/provider/user/user_remote_provider_impl.dart';
 import 'package:toplearth/data/repository/auth_repository_impl.dart';
 import 'package:toplearth/data/repository/group_repository_impl.dart';
+import 'package:toplearth/data/repository/plogging_repository_impl.dart';
 import 'package:toplearth/data/repository/user_repository_impl.dart';
 import 'package:toplearth/domain/repository/auth/auth_repository.dart';
 import 'package:toplearth/domain/repository/group/group_repository.dart';
+import 'package:toplearth/domain/repository/plogging/plogging_repository.dart';
 import 'package:toplearth/domain/repository/user/user_repository.dart';
 
 class AppDependency extends Bindings {
@@ -22,10 +26,12 @@ class AppDependency extends Bindings {
     Get.lazyPut<AuthRemoteProvider>(() => AuthRemoteProviderImpl());
     Get.lazyPut<UserRemoteProvider>(() => UserRemoteProviderImpl());
     Get.lazyPut<GroupRemoteProvider>(() => GroupRemoteProviderImpl());
+    Get.lazyPut<PloggingRemoteProvider>(() => PloggingRemoteProviderImpl());
 
     // Add your repository dependencies here
     Get.lazyPut<AuthRepository>(() => AuthRepositoryImpl());
     Get.lazyPut<UserRepository>(() => UserRepositoryImpl());
     Get.lazyPut<GroupRepository>(() => GroupRepositoryImpl());
+    Get.lazyPut<PloggingRepository>(() => PloggingRepositoryImpl());
   }
 }
