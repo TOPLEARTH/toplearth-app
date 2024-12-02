7d224330aad3b184b7adc0e5fc9916e271de4eb8
kyujenius <hkj020607@gmail.com>
2024-12-03 01:26:17 +0900

    Refactor: app_dependency.dart 추가

--- lib/app/config/app_dependency.dart
+++ lib/app/config/app_dependency.dart
@@ -4,6 +4,8 @@ import 'package:toplearth/data/provider/auth/auth_remote_provider.dart';
 import 'package:toplearth/data/provider/auth/auth_remote_provider_impl.dart';
 import 'package:toplearth/data/provider/group/group_remote_provider.dart';
 import 'package:toplearth/data/provider/group/group_remote_provider_impl.dart';
+import 'package:toplearth/data/provider/home/home_provider.dart';
+import 'package:toplearth/data/provider/home/home_provider_impl.dart';
 import 'package:toplearth/data/provider/matching/matching_remote_provider.dart';
 import 'package:toplearth/data/provider/matching/matching_remote_provider_impl.dart';
 import 'package:toplearth/data/provider/plogging/plogging_remote_provider.dart';
@@ -12,11 +14,13 @@ import 'package:toplearth/data/provider/user/user_remote_provider.dart';
 import 'package:toplearth/data/provider/user/user_remote_provider_impl.dart';
 import 'package:toplearth/data/repository/auth_repository_impl.dart';
 import 'package:toplearth/data/repository/group_repository_impl.dart';
+import 'package:toplearth/data/repository/home_repository_impl.dart';
 import 'package:toplearth/data/repository/matching_repository_impl.dart';
 import 'package:toplearth/data/repository/plogging_repository_impl.dart';
 import 'package:toplearth/data/repository/user_repository_impl.dart';
 import 'package:toplearth/domain/repository/auth/auth_repository.dart';
 import 'package:toplearth/domain/repository/group/group_repository.dart';
+import 'package:toplearth/domain/repository/home/home_repository.dart';
 import 'package:toplearth/domain/repository/matching/matching_repository.dart';
 import 'package:toplearth/domain/repository/plogging/plogging_repository.dart';
 import 'package:toplearth/domain/repository/user/user_repository.dart';
@@ -26,19 +30,19 @@ class AppDependency extends Bindings {
   void dependencies() {
     // Add your mediator dependencies here
 
-
     // Add your provider dependencies here
-    Get.lazyPut(()=>WebSocketController());
+    Get.lazyPut(() => WebSocketController());
     Get.lazyPut<AuthRemoteProvider>(() => AuthRemoteProviderImpl());
     Get.lazyPut<UserRemoteProvider>(() => UserRemoteProviderImpl());
+    Get.lazyPut<HomeRemoteProvider>(() => HomeRemoteProviderImpl());
     Get.lazyPut<GroupRemoteProvider>(() => GroupRemoteProviderImpl());
     Get.lazyPut<PloggingRemoteProvider>(() => PloggingRemoteProviderImpl());
     Get.lazyPut<MatchingRemoteProvider>(() => MatchingRemoteProviderImpl());
 
-
     // Add your repository dependencies here
     Get.lazyPut<AuthRepository>(() => AuthRepositoryImpl());
     Get.lazyPut<UserRepository>(() => UserRepositoryImpl());
+    Get.lazyPut<HomeRepository>(() => HomeRepositoryImpl());
     Get.lazyPut<GroupRepository>(() => GroupRepositoryImpl());
     Get.lazyPut<PloggingRepository>(() => PloggingRepositoryImpl());
     Get.lazyPut<MatchingRepository>(() => MatchingRepositoryImpl());
